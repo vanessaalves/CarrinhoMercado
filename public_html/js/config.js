a5dc281e94574f5223f33812e07c7a90c9b42a2e
vanessaalves <vns.alvess@gmail.com>
2018-07-17 21:00:37 -0300

    Carrinho

---
+++ public_html/js/config.js
@@ -0,0 +1,9 @@
+function setConfig(){
+    var texts = {
+        "title":"Lista Mercado"
+    };
+    document.title = texts.title;
+    document.getElementById("navTitle").innerHTML = texts.title;
+    
+}
+setConfig();
